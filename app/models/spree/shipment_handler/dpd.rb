ab7d1c2c5c04bf025bcbbef4a397bf1f9e7b79d3
mrbrdo <mrbrdo@gmail.com>
2022-05-05 15:55:25 +0200

    don't create new DPD shipment if shipment already has tracking (e.g. manually set)

--- app/models/spree/shipment_handler/dpd.rb
+++ app/models/spree/shipment_handler/dpd.rb
@@ -3,18 +3,19 @@ module Spree
     class Dpd < Spree::ShipmentHandler
       def update_order_shipment_state
         order = @shipment.order
-
         new_state = OrderUpdater.new(order).update_shipment_state
-        
-        is_being_shipped = new_state == 'shipped'
-        tracking_all = SpreeDpd::Shipment.new(@shipment).create_order_dpd
-        # TODO: there can be multiple tracking codes, handle this
-        tracking = tracking_all.first
-        @shipment.update tracking: tracking
-        
+
+        # If tracking code was manually set, don't create new DPD order
+        if @shipment.tracking.blank? && new_state == 'shipped'
+          tracking_all = SpreeDpd::Shipment.new(@shipment).create_order_dpd
+          # TODO: there can be multiple tracking codes, handle this
+          tracking = tracking_all.first
+          @shipment.update tracking: tracking
+        end
+
         order.update_columns(shipment_state: new_state, updated_at: Time.current)
       end
-      
+
       protected
       def send_shipped_email
         # Delay email a bit because DPD tracking code is not working immediately
