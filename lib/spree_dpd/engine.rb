f71430268e873461c78287d75104d72ab827657f
mrbrdo <mrbrdo@gmail.com>
2024-01-14 22:54:03 +0100

    Spree 4.7 support

--- lib/spree_dpd/engine.rb
+++ lib/spree_dpd/engine.rb
@@ -11,7 +11,20 @@ module SpreeDpd
       g.test_framework :rspec
     end
 
+    module AddDpdSubmenuToMainMenu
+      def add_integrations_section(root)
+        super
+
+        root.insert_before('integrations',
+          ::Spree::Admin::MainMenu::ItemBuilder.new('dpd', admin_dpd_path).
+          with_icon_key('box.svg').
+          with_match_path('/dpd').
+          build)
+      end
+    end
+
     def self.activate
+      ::Spree::Admin::MainMenu::DefaultConfigurationBuilder.prepend AddDpdSubmenuToMainMenu
     end
 
     config.to_prepare &method(:activate).to_proc
