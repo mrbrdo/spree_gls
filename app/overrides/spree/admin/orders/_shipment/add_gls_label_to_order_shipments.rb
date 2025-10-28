d2987874d5e187c009605d8f43e5c2557b4595fa
mrbrdo <mrbrdo@gmail.com>
2025-10-28 14:33:24 +0100

    Remove Deface-related code

--- app/overrides/spree/admin/orders/_shipment/add_gls_label_to_order_shipments.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  virtual_path:  'spree/admin/orders/_shipment',
-  name:          'add_gls_label_to_order_shipments',
-  insert_after: '.stock-location .stock-location-name',
-  partial: 'spree/admin/orders/gls_shipment_actions'
-)
